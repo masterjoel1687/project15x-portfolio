36ab42f405f2f38d9e87d8e4979586ba7c74f02f
masterjoel1687 <remallijoel@gmail.com>
2025-05-24 10:50:13 +0530

    Created Calculator

---
+++ Week-1 Python Basics/Calculator.py
@@ -0,0 +1,19 @@
+First_integer = float(input("Enter first integer: "))
+Second_integer = float(input("Enter second integer: "))
+a= First_integer
+b= Second_integer
+add= a + b
+sub= a - b
+mul= a * b
+div= a / b
+floorDiv= a // b
+mod= a % b
+exp= a ** b
+print(f"Addition of {a} + {b} : ", add)
+print(f"Subtraction of {a} + {b} : ", sub)
+print(f"Multiplication of {a} + {b} : ", mul)
+print(f"Division of {a} + {b} : ", div)
+print(f"Floor Division of {a} + {b} : ", floorDiv)
+print(f"Modulus of {a} + {b} : ", mod)
+print(f"Exponentiation of {a} + {b} : ", exp)
+print("Thank you for using the calculator!")
